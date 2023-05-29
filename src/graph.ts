91eb2678cb4f30f001bc938db6009874721aaf1e
Garm <melsom.henrik@gmail.com>
2023-05-30 00:55:32 +0200

    🚧 Transform from view to graph space

--- src/graph.ts
+++ src/graph.ts
@@ -1,4 +1,4 @@
-import { CanvasForm, CanvasSpace, Pt } from "pts";
+import { CanvasForm, CanvasSpace, Pt, Rectangle } from "pts";
 
 export class GraphRenderer {
   space: CanvasSpace;
@@ -6,28 +6,38 @@ export class GraphRenderer {
 
   view: GraphView;
 
-  constructor(elem: string | Element) {
+  margins: Margins;
+  resolution: number = 100;
+
+  constructor(elem: string | Element, margins?: Margins) {
+    this.margins = margins ?? new Margins(50);
+
     this.space = new CanvasSpace(elem);
     this.space.setup({ bgcolor: "#111" });
     this.space.add({
       animate: () => this.animate(),
       action: (type, px, py, evt) => this.action(type, px, py, evt),
     });
+
     this.form = this.space.getForm();
+
+    this.view = new GraphView(this.space, this.margins);
+
     this.space.bindMouse().bindTouch().play();
   }
 
   animate() {
     let [space, form] = [this.space, this.form];
+    this.view = new GraphView(this.space, this.margins);
 
-    let resolution = 100;
-    let step = 1 / resolution;
+    let step = 1 / (this.resolution - 1);
     let pts = [];
-    for (let i = 0; i < resolution; i++) {
-      let x = i * step * space.width;
-      let y = space.height - x;
+    for (let i = 0; i < this.resolution; i++) {
+      let x = i * step * this.view.viewSize.x + this.view.viewPos.x;
+      x = this.view.viewToGraphX(x);
+      let y = x;
 
-      pts.push(new Pt(x, y));
+      pts.push(this.view.graphToView(new Pt(x, y)));
     }
 
     form.fillOnly("#fff").points(pts, 2, "circle");
@@ -38,29 +48,82 @@ export class GraphRenderer {
     }
 
     form.strokeOnly("#fff").lines(lines);
+
+    form.rect(Rectangle.fromTopLeft(this.view.viewPos, this.view.viewSize));
+
+    let tickStepX = this.view.graphSize
   }
 
   action(type: string, px: number, py: number, evt: Event) {}
 }
 
+export class Margins {
+  left: number = 0;
+  top: number = 0;
+  right: number = 0;
+  bottom: number = 0;
+
+  constructor(
+    allOrLeftRightOrLeft?: number,
+    topBottomOrTop?: number,
+    right?: number,
+    bottom?: number
+  ) {
+    if (allOrLeftRightOrLeft == undefined) {
+      return;
+    }
+
+    if (topBottomOrTop == undefined) {
+      this.left = allOrLeftRightOrLeft;
+      this.right = allOrLeftRightOrLeft;
+      this.top = allOrLeftRightOrLeft;
+      this.bottom = allOrLeftRightOrLeft;
+      return;
+    }
+
+    if (right == undefined) {
+      this.left = allOrLeftRightOrLeft;
+      this.right = allOrLeftRightOrLeft;
+      this.top = topBottomOrTop;
+      this.bottom = topBottomOrTop;
+      return;
+    }
+
+    this.left = allOrLeftRightOrLeft;
+    this.top = topBottomOrTop;
+    this.right = right;
+    this.bottom = bottom ?? 0;
+  }
+}
+
 export class GraphView {
-  viewX: number;
-  viewY: number;
+  viewPos: Pt = new Pt();
+  viewSize: Pt = new Pt();
+  graphPos: Pt = new Pt();
+  graphSize: Pt = new Pt();
 
-  viewWidth: number;
-  viewHeight: number;
-
-  graphWidth: number;
-  graphHeight: number;
-
-  offsetX: number;
-  offsetY: number;
+  constructor(space: CanvasSpace, margins: Margins) {
+    this.viewPos = new Pt(margins.left, margins.top);
+    this.viewSize = space.size.$subtract(
+      new Pt(margins.left + margins.right, margins.top + margins.bottom)
+    );
+    this.graphPos = new Pt(0, 0);
+    this.graphSize = new Pt(1, 1);
+  }
 
   viewToGraph(pt: Pt): Pt {
-    return pt
-      .$subtract(new Pt(this.viewX, this.viewY))
-      .$divide(new Pt(this.viewWidth, this.viewHeight))
-      .$multiply(new Pt(this.graphWidth, this.graphHeight))
-      .$add(new Pt(this.offsetX, this.offsetY));
+    let normalized = pt.$subtract(this.viewPos).$divide(this.viewSize);
+    normalized.y = 1 - normalized.y;
+    return normalized.$multiply(this.graphSize).$add(this.graphPos);
+  }
+
+  viewToGraphX(x: number): number {
+    return this.viewToGraph(new Pt(x, 0)).x;
+  }
+
+  graphToView(pt: Pt): Pt {
+    let normalized = pt.$subtract(this.graphPos).$divide(this.graphSize);
+    normalized.y = 1 - normalized.y;
+    return normalized.$multiply(this.viewSize).$add(this.viewPos);
   }
 }
